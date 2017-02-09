248136acfc6ec84052b6cb01b5cad29a19d8384f
wangweitao04 <wangweitao04@meituan.com>
2017-02-09 10:33:13 +0800

    feature: init demo code of activityRouter_chenenyu.

---
+++ Demo_ActivityRouter/app/src/main/java/wwtao/demo/demo_activityrouter/activities/ActivityF.java
@@ -0,0 +1,20 @@
+package wwtao.demo.demo_activityrouter.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.Serializable;
+
+/**
+ * Created by wangweitao04 on 17/2/8.
+ */
+
+public class ActivityF extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
